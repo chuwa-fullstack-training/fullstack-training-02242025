d7dcf0ae30aba79434399f84306e5cb5955927ad
Jian <kylofive@outlook.com>
2025-02-27 15:21:35 -0800

    branch test

--- homeworks/lecture2/hw1.js
+++ homeworks/lecture2/hw1.js
@@ -1,4 +1,6 @@
 /*
+test branch
+
 * Copy the enumerable properties of p to o, and return o.
 * If o and p have a property by the same name, o's property is overwritten.
 * This function does not handle getters and setters or copy attributes.

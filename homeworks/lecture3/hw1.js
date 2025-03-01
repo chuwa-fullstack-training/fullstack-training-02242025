037284ed05c5ec2badb2f80a4003e73c126dc767
hjl667 <hongji667719@gmail.com>
2025-02-28 19:16:48 -0800

    pick coins

--- homeworks/lecture3/hw1.js
+++ homeworks/lecture3/hw1.js
@@ -5,4 +5,26 @@
  */
 function pickCoins() {
     // implement here
+    const set = new Set();
+    const coins = [1, 5, 25, 50];
+    const stringifyResult= (arr) => arr.map((count, i) => `${coins[i]}c: ${count}`).join(", ");
+
+    const dfs=(arr, sum, count, currentIndex) =>{
+        if(sum > 100 || count > 48) return;
+        if(count === 48 && sum === 100){
+            set.add(stringifyResult(arr));
+            if (set.size >= 2) return;
+        }
+        for(let i = currentIndex; i < coins.length; i++){
+            arr[i]++;
+            dfs(arr, sum + coins[i], count+1, i);
+            arr[i]--;
+            if (set.size >= 2) return;
+        }
+    }
+    dfs(new Array(4).fill(0), 0, 0, 0);
+    return set;
 }
+
+const res = pickCoins();
+res.forEach((value)=>console.log(value));

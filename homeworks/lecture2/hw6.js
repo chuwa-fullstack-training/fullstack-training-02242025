f06067d313c1076e382ae2952654c9b64ac0f483
chenguanghe1992 <chenguanghe1992@gmail.com>
2025-03-02 14:27:24 -0800

    Update hw6.js

--- homeworks/lecture2/hw6.js
+++ homeworks/lecture2/hw6.js
@@ -3,14 +3,26 @@
 // 1. Write a function that returns the largest element in a list.
 function largestElement(list) {
     // implement your code here
+     return Math.max(list);
 }
 
 // 2. Write function that reverses a list, preferably in place.
 function reverseList(list) {
     // implement your code here
+    return list.reverse();
 }
 
 // 3. Write a function that checks whether an element occurs at least twice in a list.
 function checkTwice(list, element) {
     // implement your code here
+    let count = 0;
+    for(let i in list){
+        if(i===element){
+            count++;
+        }
+        if(count==2){
+            return true;
+        }
+    }
+    return false; 
 }

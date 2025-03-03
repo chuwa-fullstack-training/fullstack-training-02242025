0c60cb846b2412aa13cc0c810daba00ddaf72c09
manda <100242191+mandali8686@users.noreply.github.com>
2025-03-03 13:14:33 -0600

    Update hw1.js

--- homeworks/lecture2/hw1.js
+++ homeworks/lecture2/hw1.js
@@ -4,15 +4,23 @@
 * This function does not handle getters and setters or copy attributes.
 */
 function extend(o, p) {
-    // implement your code here
+    for (let key in p){
+        o[key] = p[key];
+    }
+    return o;
 }
+
 
 /*
 * Return a new object that holds the properties of both o and p.
 * If o and p have properties by the same name, the values from o are used.
 */
 function union(o, p) {
-    // implement your code here
+    let new_obj=o;
+    for (let key in p){
+        new_obj[key] = o[key]?o[key]:p[key];
+    }
+    return new_obj;
 }
 
 /*
@@ -20,14 +28,24 @@ function union(o, p) {
 * Return o.
 */
 function restrict(o, p) {
-    // implement your code here
+    for (let key in o){
+        if (!Object.keys(p).includes(key)){
+            delete o[key];
+        }
+    }
+    return o;
 }
-
 /*
 * Return a new object that holds only the properties of o that also appear
 * in p. This is something like the intersection of o and p, but the values of
 * the properties in p are discarded
 */
 function intersection(o, p) {
-    // implement your code here
+    let new_obj={};
+    for (let key in o){
+        if (Object.keys(p).includes(key)){
+            new_obj[key] = o[key]
+        }
+    }
+    return new_obj
 }

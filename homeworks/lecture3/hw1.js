26c02cf4c95a4aa74481085ee653e6782fefd109
chenguanghe1992 <chenguanghe1992@gmail.com>
2025-03-03 00:08:31 -0800

    Update hw1.js

--- homeworks/lecture3/hw1.js
+++ homeworks/lecture3/hw1.js
@@ -11,9 +11,9 @@ function pickCoins() {
             for (let c = 0; c <= 4; c++) {
                 for (let d = 0; d <= 2; d++) {
                     let sum = a * 1 + b * 5 + c * 25 + d * 50;
-                    if (sum === 100) {
+                    if (sum === 100 && (a+b+c+d<=48) {
                         output.push([a, b, c, d]);
-                    } else if (sum > 100) {
+                    } else if (sum > 100 || (a+b+c+d>48) {
                         break;
                     }
                 }

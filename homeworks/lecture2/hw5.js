f87517b27711937b476c8aaf958ab1f29643cd21
manda <100242191+mandali8686@users.noreply.github.com>
2025-03-03 16:00:38 -0600

    Update hw5.js

--- homeworks/lecture2/hw5.js
+++ homeworks/lecture2/hw5.js
@@ -14,6 +14,8 @@ if (x !== 3) {
 if (x === 3) {
   console.log(y);
 }
+//The declaration of y will be moved to the top, so when x !==3, y=5, then y===5 is true, x=3
+//First will print 5, second will print 5; x=3 is declared inside y==5 condition, the outside x===3 not true, so only 2 print happens.
 
 
 // 2.
@@ -23,4 +25,5 @@ if (x === 3) {
   console.log(x);
 }
 console.log(x);
-
+//x=3, so x===3 is true, var x=2 under this condition, the first print 2.
+//The x is redeclared to 2, the second print 2.

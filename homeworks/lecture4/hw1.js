05fd25cadfa0c42f781f4b86de0c40cb03af4b71
manda <100242191+mandali8686@users.noreply.github.com>
2025-03-06 12:54:48 -0600

    Update hw1.js

--- homeworks/lecture4/hw1.js
+++ homeworks/lecture4/hw1.js
@@ -7,4 +7,22 @@
 
 function checkValidHTML(html) {
     // implement your solution here
+    let tag_list = [];
+    let regex = /<\/?([a-zA-Z0-9]+)[^>]*>/g;
+    let match;
+     while ((match = regex.exec(html)) !== null) {
+         // console.log(match);
+         let tag = match[1];
+         if (match[0][1] !== '/'){
+             tag_list.push(tag);
+         }
+         else{
+             if (tag_list.length===0 || tag_list.pop() !== tag){
+                 return false;
+             }
+         }
+     }
+    return tag_list.length===0;
+    
+    
 }

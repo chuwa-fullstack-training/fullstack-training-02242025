69786570386c9b32c3284224119b642ecf524026
manda <100242191+mandali8686@users.noreply.github.com>
2025-03-06 16:31:22 -0600

    Update hw2.js

--- homeworks/lecture5/hw2.js
+++ homeworks/lecture5/hw2.js
@@ -9,9 +9,23 @@
  */
 function reverseWords(str) {
   // your code here
-  let string_join = str.join(''); 
-  let str_list = string_join.split(" ").reduce((acc, word) => word + " " + acc, "").trim();
-  return str_list;
+  reverse(str, 0, str.length-1);
+  let w1=0;
+  for (let w2=0; w2<=str.length; w2++){
+    if (str[w2]==' ' || w2===str.length){
+      reverse(str, w1, w2-1);
+      w1=w2+1;
+    }
+  }
+}
+function reverse(arr, left, right){
+  while (left<right){
+    let temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+    left ++;
+    right --;
+  }
 }
 
 const input = 'the sky is blue'.split(''); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']

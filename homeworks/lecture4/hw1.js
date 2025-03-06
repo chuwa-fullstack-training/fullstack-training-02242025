217ce4962fb9a5ca46398aa9568c8e0e660c0778
chenguanghe1992 <chenguanghe1992@gmail.com>
2025-03-05 22:57:35 -0800

    Update hw1.js

--- homeworks/lecture4/hw1.js
+++ homeworks/lecture4/hw1.js
@@ -7,4 +7,21 @@
 
 function checkValidHTML(html) {
     // implement your solution here
+    const tagPattern = /<\/?([a-zA-Z]+)[^>]*>/g;
+    let stack = [];
+    let match;
+    while ((match = tagPattern.exec(html)) !== null) {
+        let tag = match[1];
+
+        if (match[0][1] !== '/') {
+            // Opening tag, push to stack
+            stack.push(tag);
+        } else {
+            // Closing tag, check stack
+            if (stack.length === 0 || stack.pop() !== tag) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
 }

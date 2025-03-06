59626961be590967339951217b46902114dbda46
manda <100242191+mandali8686@users.noreply.github.com>
2025-03-06 10:46:53 -0600

    Update hw1.js

--- homeworks/lecture3/hw1.js
+++ homeworks/lecture3/hw1.js
@@ -5,4 +5,24 @@
  */
 function pickCoins() {
     // implement here
+    let coins=[50, 25, 5, 1];
+    let target = 100;
+    let pick = 48;
+    let result = [];
+    function dfs(path, remain, pick_remain, index){
+        if (result.length == 2) return;
+        if (remain == 0 && pick_remain ==0){
+            result.push([...path]);
+            return;
+        }
+        if (remain<0 || pick_remain<0) return;
+
+        for (let i = index; i < coins.length; i++){
+            path.push(coins[i])
+            dfs(path, remain - coins[i], pick_remain -1, i);
+            path.pop();
+        }
+    }
+    dfs([], target, pick, 0)
+    return result;
 }

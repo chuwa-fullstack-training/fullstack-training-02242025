5c28e15fab241a7003827c441b3216820ea221ed
ruisiw <rw2902@columbia.edu>
2025-03-06 18:39:40 -0500

    hmw

--- homeworks/lecture4/hw5.js
+++ homeworks/lecture4/hw5.js
@@ -8,4 +8,20 @@
 
 const cloneDeepWithLoop = (obj) => {
     // Implement the function here
-}
+    const map = new Map();
+    const clone = (obj) => {
+        if (typeof obj !== 'object' || obj === null) return obj;
+        if (map.has(obj)) {
+            return map.get(obj);
+        }
+        const result = Array.isArray(obj) ? [] : Object.create(Object.getPrototypeOf(obj));
+        map.set(obj, result);
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                result[key] = clone(obj[key]);
+            }
+        }
+        return result;
+    };
+    return clone(obj);
+};

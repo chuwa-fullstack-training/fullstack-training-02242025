b440649b965eb9b7671c803e5dcd6a1f7927e5f8
hjl667 <hongji667719@gmail.com>
2025-03-05 22:00:51 -0800

    hw5 q2

--- homeworks/lecture5/hw2.js
+++ homeworks/lecture5/hw2.js
@@ -8,8 +8,23 @@
  * @param {string[]} str
  */
 function reverseWords(str) {
-  // your code here
+  const reverse = (start, end) => {
+    while(start < end){
+      [str[start], str[end]] = [str[end], str[start]];
+      start++;
+      end--;
+    }
+  }
+  reverse(0, str.length - 1);
+  for(let left = 0, right = 0; right <= str.length; right++){
+    if(right === str.length || str[right] === " "){
+      reverse(left, right-1);
+      left = right+1;
+    }
+  }
+  return str;
 }
 
 const input = 'the sky is blue'.split(''); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']
 reverseWords(input);
+console.log(input.join(""));

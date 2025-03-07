2498f8e97ca3ab47b0341435a6caa00a609a1781
manda <100242191+mandali8686@users.noreply.github.com>
2025-03-07 10:17:39 -0600

    Update hw3.js

--- homeworks/lecture5/hw3.js
+++ homeworks/lecture5/hw3.js
@@ -11,7 +11,7 @@ new Promise((resolve, reject) => {
 }).then(result => console.log(result));
 //a,c,e,d,b.
 //First the JS queue get executed and the Promise will be treated as Microtask, so all the console.log is printed except the one in setTimeOut and Promise result, printed out a,c,e
-//Then the Microtask get executed first, because setTimeOut is Web API and will be added in Callback queue and executed after Microtask. 
+//Then the Microtask get executed first, because setTimeOut is Web API and will be added in Callback queue and executed after Microtask queue. 
 
 // 2
 const fn = () =>
@@ -26,4 +26,4 @@ fn().then(res => {
 
 console.log('start');
 // 1, start, success
-//When fn is invocated, if will print 1 first and then save the result 'success' as Microtask in Callback queue, then the console.log('start') get executed, after that the Microtask get executed and print 'success'.
+//When fn is invocated, if will print 1 first and then save the result 'success' as Microtask in Microtask queue, then the console.log('start') get executed, after that the Microtask get executed and print 'success'.

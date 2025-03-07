d95cf2cb6619e2387067709de168542c7947cd3f
Stella <sun.weiz@northeastern.edu>
2025-03-07 13:32:33 -0800

    hw-lecture5

--- homeworks/lecture5/hw4.js
+++ homeworks/lecture5/hw4.js
@@ -1,33 +1,36 @@
 // what is the output? and explain why?
 
-// 1
-Promise.resolve(1)
-  .then(res => {
-    console.log(res);
-    return 2;
+// 1 Anwer: 1, 2
+Promise.resolve(1) // promise resolve with 1
+  .then(res => { //schedule .then()
+    console.log(res); // print 1
+    return 2; // now res is 2
   })
-  .catch(err => {
+  .catch(err => { // skip
     return 3;
   })
-  .then(res => {
-    console.log(res);
+  .then(res => { // apply .then() with res = 2
+    console.log(res); // print 2
   });
-
-// // 2
-Promise.reject(1)
-  .then(res => {
-    console.log(res);
+ 
+//2 Answer: 1, 3
+Promise.reject(1) // promise reject with 1
+  .then(res => { //won't be executed
+    console.log(res); 
     return 2;
   })
-  .catch(err => {
-    console.log(err);
-    return 3;
+  .catch(err => { // schedule .catch()
+    console.log(err); // print 1
+    return 3; // now err is 3
   })
-  .then(res => {
-    console.log(res);
+  .then(res => { // apply .then() with res = 3
+    console.log(res); // print 3
   });
 
-//3
+  
+//3 Answer: ["Error: 2"]
+
+// function runAsync() returns a promise that resolves with value x after 1 second 
 function runAsync(x) {
   const p = new Promise(resolve =>
     setTimeout(() => resolve(x), 1000)
@@ -35,6 +38,7 @@ function runAsync(x) {
   return p;
 }
 
+// function runReject() returns promise that reject with value x after x seconds
 function runReject(x) {
   const p = new Promise((resolve, reject) =>
     setTimeout(() => reject(`Error: ${x}`), 1000 * x)
@@ -42,6 +46,8 @@ function runReject(x) {
   return p;
 }
 
+// when there is promise got rejected, promise.all() will catch the first error and return it
+// so the output will be ["Error: 2"]
 Promise.all([runAsync(1), runReject(4), runAsync(3), runReject(2)])
   .then(res => console.log(res))
   .catch(err => console.log(err));

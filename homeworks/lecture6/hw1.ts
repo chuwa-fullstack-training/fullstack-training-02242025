60e64a69dc8a13e765f09a1882bec9af3bebcb7a
chenguanghe1992 <chenguanghe1992@gmail.com>
2025-03-09 23:16:17 -0700

    Update hw1.ts

--- homeworks/lecture6/hw1.ts
+++ homeworks/lecture6/hw1.ts
@@ -10,6 +10,13 @@ function makeCustomer<T extends User>(u: T): T {
     type: "customer",
   };
 }
+//T is a subtype of user, so t may have other properties that user doesn't have . typeScript doesn't support return child class without including all properteis
+function makeCustomer<T extends User>(u: T): T {
+  return {
+    ...u,
+    type: "customer",
+  };
+}
 
 // 2. fix the following code
 // requirement: the function should accept either two strings or two numbers at the same time,
@@ -21,3 +28,10 @@ function f(a: string | number, b: string | number) {
     return a + b;
   }
 }
+//may throw error is a is string and b is a number
+if (typeof a === "string" && typeof b === "string") {
+    return `${a} : ${b}`;
+  } 
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  }

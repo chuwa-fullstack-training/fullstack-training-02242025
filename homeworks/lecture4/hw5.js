600cfe97677b2b4776271dd82681198dc87fe215
kiki614zzzz <131100606+kiki614zzzz@users.noreply.github.com>
2025-03-13 17:24:21 -0400

    Update hw5.js

--- homeworks/lecture4/hw5.js
+++ homeworks/lecture4/hw5.js
@@ -6,6 +6,30 @@
 // }
 // data.child = data;
 
-const cloneDeepWithLoop = (obj) => {
+const cloneDeepWithLoop = (obj, visited = new WeakMap()) => {
     // Implement the function here
+    if (typeof obj !== 'object' || obj === null) {
+        return obj;
+    }
+    if (visited.has(obj)) {
+        return visited.get(obj);
+    }
+    let cloneObj = Array.isArray(obj) ? [] : {};
+    visited.set(obj, cloneObj);
+
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            cloneObj[key] = cloneDeepWithLoop(obj[key], visited);
+        }
+    }
+    return cloneObj;
 }
+const data = {
+    name: 'foo',
+    child: null
+}
+data.child = data; 
+const clonedData = cloneDeepWithLoop(data);
+console.log(clonedData);
+console.log(clonedData.child === clonedData); 
+console.log(clonedData !== data);

870fd6d39cb75b3cca45873d7f92aad41470fba8
manda <100242191+mandali8686@users.noreply.github.com>
2025-03-13 14:15:59 -0500

    Update hw1.ts

--- homeworks/lecture6/hw1.ts
+++ homeworks/lecture6/hw1.ts
@@ -6,18 +6,26 @@ type User = {
 
 function makeCustomer<T extends User>(u: T): T {
   return {
-    id: u.id,
+    // id: u.id,
+    ...u,
     type: "customer",
   };
 }
+// T here is a type that extends User, and it can have more properties than the ones in User. 
+// u instantiated T, so u may not only have "id" and "type" attributes. If the additional attributes are not preserved, there will type mismatching.
+// Spread Operator ...u make sure all the attributes are preserved.
+
 
 // 2. fix the following code
 // requirement: the function should accept either two strings or two numbers at the same time,
 // so if parameters are one string and one number, it should throw an error
-function f(a: string | number, b: string | number) {
+function f(a: string | number, b: string | number): string | number{
+  if (typeof a !== typeof b){
+    throw new Error('Parameters must be both number or both string.')
+  }
   if (typeof a === "string") {
     return `${a} : ${b}`;
   } else {
-    return a + b;
+    return (a as number) + (b as number);
   }
 }

8176d091b3dc15d24a99fee233a57ae343db71a2
kiki614zzzz <131100606+kiki614zzzz@users.noreply.github.com>
2025-03-13 11:43:53 -0400

    Update hw5.js

--- homeworks/lecture3/hw5.js
+++ homeworks/lecture3/hw5.js
@@ -13,4 +13,27 @@
  */
 function User() {
     // implement here
+    let password = "";
+    this.setPassword = function (newPassword){
+        if (newPassword.length < 6) {
+            throw new Error("Error");
+        }
+        password = newPassword;
+    };
+    
+    this.checkPassword = function(inputPassword) {
+        return inputPassword === password;
+    };
 }
+
+const user = new User();
+user.setPassword('123456');
+console.log(user.checkPassword('123456')); 
+console.log(user.checkPassword('123')); 
+try {
+    user.setPassword('123');
+} catch(error) {
+    console.log(error.message);
+};
+console.log(user.setPassword('123'));
+console.log(user.password);

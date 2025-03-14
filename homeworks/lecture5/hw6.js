c9c9c225407298e819dc1160134f7aedb2d1cce6
kiki614zzzz <131100606+kiki614zzzz@users.noreply.github.com>
2025-03-13 20:25:36 -0400

    hw1

--- homeworks/lecture5/hw6.js
+++ homeworks/lecture5/hw6.js
@@ -12,19 +12,29 @@ function sequencePromise(urls) {
     return getJSON(url).then(response => results.push(response));
   }
   // implement your code here
-
-  return results;
+  return urls.reduce((promiseChain, url) => {
+    return promiseChain.then(() => fetchOne(url));
+  }, Promise.resolve())
+  .then(() => results); // Return results after all requests complete
 }
+
+ 
 
 // option 1
-function getJSON(url) {
-  // this is from hw5
-}
+// function getJSON(url) {
+//   // this is from hw5
+// }
 
 // option 2
-// function getJSON(url) {
-//     return fetch(url).then(res => res.json());
-// }
+function getJSON(url) {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    });
+}
 
 // test your code
 const urls = [
@@ -32,3 +42,7 @@ const urls = [
   'https://api.github.com/search/repositories?q=react',
   'https://api.github.com/search/repositories?q=nodejs'
 ];
+
+sequencePromise(urls)
+  .then(results => console.log(results.map(res => res.items.length))) // Expected output: [30, 30, 30]
+  .catch(err => console.error(err.message));

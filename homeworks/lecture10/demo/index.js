f7f640beda5503069641ce6ba45c1c8720c0c887
yinjiao <fyj121322@gmail.com>
2025-03-18 16:17:08 -0700

    hm10

--- homeworks/lecture10/demo/index.js
+++ homeworks/lecture10/demo/index.js
@@ -5,32 +5,14 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const connect = require('./mongoconnect');
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-const todos = [
-  { id: 1, todo: 'first thing', done: true },
-  { id: 2, todo: 'second thing', done: false },
-  { id: 3, todo: 'third thing', done: false }
-];
+const toDoList = require('./routers/todo');
 
-app.get('/', (req, res) => {
-  res.render('index', { todos });
-});
-
-app.post('/api/todos', (req, res) => {
-  const todo = req.body.todo;
-  todos.push({ id: todos.length + 1, todo, done: false });
-  res.json(todos);
-});
-
-app.put('/api/todos/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const todo = todos.find(t => t.id === id);
-  todo.done = !todo.done;
-  res.json(todo);
-});
+app.use('/', toDoList);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');

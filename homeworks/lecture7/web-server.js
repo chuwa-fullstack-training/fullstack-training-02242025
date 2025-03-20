9b36fcd9275da2ae73b20adfcf1efd5861ed7d22
Stella <sun.weiz@northeastern.edu>
2025-03-20 11:51:34 -0700

    update hw7

--- homeworks/lecture7/web-server.js
+++ homeworks/lecture7/web-server.js
@@ -1,7 +1,18 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-
+const querystring = require('querystring');
+/**
+ * For sample code web-server.js, make the following changes:
+ * Once submitting in home.html, stay on the same page and display the submitted data.
+ * 
+ * Hint:
+ * 1. put the data of the submitted form in the query string of the url
+ * 2. before res.end() in POST method, redirect to the home.html page with the query string
+ *  - i.e. res.statusCode = 302; res.setHeader('Location', '/home.html?name=John&age=20');
+ * 3. you need to figure out how to parse the query string in the home.html page
+ * 4. after writing the html content, you need to write the query string in the html as well
+ */
 const server = http.createServer((req, res) => {
   const { url, method } = req;
   if (method === 'GET') {
@@ -30,7 +41,13 @@ const server = http.createServer((req, res) => {
       });
       req.on('end', () => {
         const parsedBody = Buffer.concat(body).toString();
-        res.end(parsedBody);
+        const formData = querystring.parse(parsedBody)
+        const queryString = Object.keys(formData)
+          .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(formData[key])}`)
+          .join('&');
+        res.statusCode = 302;
+        res.setHeader('Location', `/home.html?${queryString}`);
+        res.end();
       });
     } else {
       res.end('this is the 404 page');

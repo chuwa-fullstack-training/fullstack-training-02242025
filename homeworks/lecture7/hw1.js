217703e9a088919f20378b560db5b0c90b3a25df
manda <100242191+mandali8686@users.noreply.github.com>
2025-03-26 18:58:12 -0500

    Update hw1.js

--- homeworks/lecture7/hw1.js
+++ homeworks/lecture7/hw1.js
@@ -14,3 +14,48 @@
  */
 
 // your code here
+/**
+ * write a program that prints a list of files in the given directory, filtered by the extension of the files.
+ * The first argument is the directory name and the second argument is the extension filter.
+ * Print the list of files (one file per line) to the console.
+ *
+ * HINTS:
+ * 1. Use fs.readdir() method to read the contents of a directory.
+ * 2. Use path.extname() method to get the extension of a file. (optional)
+ * 3. Use process.argv to get command-line arguments.
+ *  - process.argv[0] is the path to the node program
+ *  - process.argv[1] is the path to the script file
+ *  - process.argv[2] is the first command-line argument
+ *    e.g. node hw1.js currentDir txt - process.argv[2] is `currentDir`, process.argv[3] is `txt`
+ */
+
+// your code here
+const { log } = require('console');
+const fs = require('fs');
+const path = require('path');
+
+const directory = process.argv[2];
+const extension = process.argv[3];
+
+if (!directory || ! extension){
+    console.error(`Use node script.js <directory> <extension>`);
+    process.exit(1);
+}
+
+fs.readdir(directory, (err, files) =>{
+    if (err){
+        console.error(`Error reading directory: ${err.message}`);
+        return;
+    }
+
+    const filteredFiles = files.filter(file =>{
+        const fileExt = path.extname(file).toLowerCase();
+        const target = extension.startsWith('.')? extension.toLowerCase():`.${extension.toLowerCase()}`;
+        return fileExt === target;
+    });
+
+    filteredFiles.forEach(file => console.log(file));
+
+})
+
+

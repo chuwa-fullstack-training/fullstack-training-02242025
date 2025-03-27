a0d6350032866eae9b34a234cfe549314c9adb51
manda <100242191+mandali8686@users.noreply.github.com>
2025-03-27 13:04:02 -0500

    Update hw3.js

--- homeworks/lecture7/hw3.js
+++ homeworks/lecture7/hw3.js
@@ -9,3 +9,64 @@
  * 3. you need to figure out how to parse the query string in the home.html page
  * 4. after writing the html content, you need to write the query string in the html as well
  */
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const querystring = require('querystring');
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+  if (method === 'GET') {
+    if (url === '/') {
+      res.end('this is the home page');
+    } else if (url === '/about') {
+      res.end('this is the about page');
+    } else if (url.startsWith('/home.html')) {
+      fs.readFile(path.join(__dirname, 'home.html'), (err, html) => {
+        if (err) {
+          res.end('error');
+          return;
+        } 
+        const query =url.split('?')[1] || '';
+        const params = querystring.parse(query);
+        let htmlString = html.toString();
+        if (params.title || params.content){
+            const display = `
+            <div>
+            <p>Title: ${params.title || ""}</p>
+            <p>Content: ${params.content || ""}</p>
+            </div>
+            `;
+            htmlString = htmlString.replace('</body>', `${display}</body>`)
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(htmlString);
+      });
+    } else {
+      res.end('this is the 404 page');
+    }
+  } else if (method === 'POST') {
+    if (url === '/create-post') {
+      let body = [];
+      req.on('data', chunk => {
+        body.push(chunk);
+      });
+      req.on('end', () => {
+        const parsedBody = querystring.parse(Buffer.concat(body).toString());
+        res.statusCode = 302;
+        res.setHeader('Location', 
+            `/home.html?title=${encodeURIComponent(parsedBody.title || "")}&&content=${encodeURIComponent(parsedBody.content || "")}`)
+        res.end();
+      });
+    } else {
+      res.end('this is the 404 page');
+    }
+  } else {
+    res.end('Unsupported method');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});

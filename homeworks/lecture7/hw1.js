9cf2e81b965636001aed6730feb56e08738fe71d
chenguanghe1992 <chenguanghe1992@gmail.com>
2025-04-09 23:51:24 -0700

    Update hw1.js

--- homeworks/lecture7/hw1.js
+++ homeworks/lecture7/hw1.js
@@ -14,3 +14,20 @@
  */
 
 // your code here
+
+const fs = require('fs');
+const path = require('path');
+const dirPath = process.argv[2];
+const extFilter = '.' + process.argv[3];
+fs.readdir(dirPath, (err, files) => {
+  if (err) {
+    return console.error('Error reading directory:', err.message);
+  }
+
+  // Filter and print files with the given extension
+  files.forEach(file => {
+    if (path.extname(file) === extFilter) {
+      console.log(file);
+    }
+  });
+});

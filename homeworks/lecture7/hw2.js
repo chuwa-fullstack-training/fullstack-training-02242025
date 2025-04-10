e1ebf90a35568a18b137d9623cc154adce90a00f
chenguanghe1992 <chenguanghe1992@gmail.com>
2025-04-09 23:52:28 -0700

    Update hw2.js

--- homeworks/lecture7/hw2.js
+++ homeworks/lecture7/hw2.js
@@ -19,3 +19,37 @@
  */
 
 // your code here
+const http = require('http');
+const url = require('url');
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true); 
+  const pathname = parsedUrl.pathname;
+  const iso = parsedUrl.query.iso;
+
+  let result;
+
+  if (pathname === '/api/parsetime') {
+    const date = new Date(iso);
+    result = {
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      second: date.getSeconds()
+    };
+  } else if (pathname === '/api/unixtime') {
+    const date = new Date(iso);
+    result = {
+      unixtime: date.getTime()
+    };
+  }
+
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(result));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
+server.listen(8000, () => {
+  console.log('Server running on http://localhost:8000');
+});

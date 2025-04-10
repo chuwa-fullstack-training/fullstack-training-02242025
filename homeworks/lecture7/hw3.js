641ef1ef397be171ea68e3c73a70b4648f82a33a
chenguanghe1992 <chenguanghe1992@gmail.com>
2025-04-09 23:56:54 -0700

    Update hw3.js

--- homeworks/lecture7/hw3.js
+++ homeworks/lecture7/hw3.js
@@ -9,3 +9,47 @@
  * 3. you need to figure out how to parse the query string in the home.html page
  * 4. after writing the html content, you need to write the query string in the html as well
  */
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const querystring = require('querystring');
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  if (req.method === 'GET' && parsedUrl.pathname === '/home.html') {
+    fs.readFile('home.html', (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Server Error');
+        return;
+      }
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+
+  } else if (req.method === 'POST' && parsedUrl.pathname === '/submit') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk;
+    });
+
+    req.on('end', () => {
+      const formData = querystring.parse(body); // { name: 'John', age: '20' }
+      const query = querystring.stringify(formData);
+
+      res.statusCode = 302;
+      res.setHeader('Location', `/home.html?${query}`);
+      res.end();
+    });
+
+  } else {
+    res.writeHead(404);
+    res.end('Page not found');
+  }
+});
+
+server.listen(8000, () => {
+  console.log('Server is running at http://localhost:8000/home.html');
+});
